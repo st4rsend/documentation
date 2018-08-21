a5bfd42ffb9552d281d08632708312ef9239c228
psychotictac <psychotictac@gmail.com>
2018-08-21 13:34:01 +0000

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule, MatToolbarModule, MatInputModule, MatIconModule } from '@angular/material';
+
+import { WebSocketService } from './shared/services/websocket.service';
+
 import { AppComponent } from './app.component';
+import { InputBoxComponent } from './input-box/input-box.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputBoxComponent,
+    TodoListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+		FormsModule,
+		BrowserAnimationsModule,
+		MatToolbarModule,
+		MatInputModule,
+		MatIconModule,
+		MatButtonModule
   ],
-  providers: [],
+  providers: [
+		WebSocketService
+	],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
