05a981f89cd936c179387a3dd0717e80862dd097
psychotictac <psychotictac@gmail.com>
2020-03-02 19:22:11 +0000

    plus root websocket server

--- websocket/websocket_server.go
+++ websocket/websocket_server.go
@@ -3,16 +3,23 @@ package main
 import (
 	"strconv"
 	"log"
-	"golang.org/x/net/websocket"
 	"net/http"
 	"st4rsend"
 )
 
 func main() {
 	var wsPort int = 8000
+	var globalLimiters = st4rsend.GlobalLimiter{
+		UserCreationRate: 10000,
+		UserCreationGranted: true,
+		UserLoginRate: 1000,
+		UserLoginGranted: true,
+	}
+
 	wsPortAscii := "localhost:" + strconv.Itoa(wsPort)
-	log.Printf("Starting http server on %s", wsPortAscii)
-	http.Handle("/ws", websocket.Handler(st4rsend.WsHandler))
+	log.Printf("Starting http server on %s\n", wsPortAscii)
+	//http.HandleFunc("/ws", st4rsend.WsHandler)
+	http.Handle("/ws", &st4rsend.SpecificHandler{Limiter: globalLimiters})
 	err := http.ListenAndServe(wsPortAscii, nil)
 	st4rsend.CheckErr(err)
 }
